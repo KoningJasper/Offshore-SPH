46aa538c182abc5e435509e5fb06c4328d35c175
KoningJasper <jasper@koning.io>
2019-01-21 14:32:37 +0100

    Fix running.

--- run.py
+++ run.py
@@ -1,30 +1,33 @@
-import argparse
+import argparse, sys
 import examples.dam_break_complete
 
+def test(args):
+    raise print('Testing hasn\'t been implemented yet.')
 
-# Commands
-def test(file: str):
-    raise Exception('Testing hasn\'t been implemented yet.')
-
-def run(file: str):
-    if file.lower() == 'dambreak':
+def run(args):
+    if args.file.lower() == 'dambreak':
         examples.dam_break_complete.main()
     else:
-        raise Exception('Invalid file to run.')
+        print('Invalid file to run.')
 
 def main():
     # create the top-level parser
-    parser = argparse.ArgumentParser(description='The main entry point for the Offshore-SPH project. Using this runner example tests can be run or the test framework can be run, to verify the integrity of the project.')
+    parser = argparse.ArgumentParser(prog='Runner', description='The main entry point for the Offshore-SPH project. Using this runner example tests can be run or the test framework can be run, to verify the integrity of the project.')
     subparsers = parser.add_subparsers()
 
-    test = subparsers.add_parser('test', help='Run tests for the project.')
-    test.add_argument('file', type=str, help='The test to run.', required=True)
-    run = subparsers.add_parser('run', help='Run one of the many examples.')
-    run.add_argument('file', type=str, help='The file to run, currently the only file that can be run is the dambreak.', required=True)
+    # Test
+    parser_test = subparsers.add_parser('test', help='Run tests for the project.')
+    parser_test.add_argument('file', type=str, help='The test to run.')
+    parser_test.set_defaults(func=test)
 
+    # Run
+    parser_run = subparsers.add_parser('run', help='Run one of the many examples.')
+    parser_run.add_argument('file', type=str, help='The file to run, currently the only file that can be run is the dambreak.')
+    parser_run.set_defaults(func=run)
+
+    # Execute the argument
     args = parser.parse_args()
-    print(args.accumulate(args.run))
-
+    args.func(args)
 
 if __name__ == '__main__':
     main()
