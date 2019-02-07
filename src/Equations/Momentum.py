2fa8701cbc4ec339b2f7904f4364d7e3093794e8
KoningJasper <jasper@koning.io>
2019-02-07 09:52:46 +0100

    Momentum partial.

--- src/Equations/Momentum.py
+++ src/Equations/Momentum.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import vectorize, jit, prange, njit
 from typing import List
 from src.Particle import Particle
 
@@ -6,15 +7,13 @@ class Momentum():
     """ Monaghan momentum equation. """
     alpha: float
     beta: float
-    eta: float
 
-    def __init__(self, alpha: float = 0.01, beta: float = 0.0, eta: float = 0.5):
+    def __init__(self, alpha: float = 0.01, beta: float = 0.0):
         # Monaghan parameters
         self.alpha = alpha
         self.beta = beta
-        self.eta = eta
 
-    def calc(self, mass: float, p: Particle, xij: np.array, rij: np.array, vij: np.array, pressure: np.array, rho: np.array, hij: np.array, cij: np.array, wij: np.array, dwij: np.array, xsph: bool) -> List:
+    def calc(self, m: np.array, p: Particle, xij: np.array, rij: np.array, vij: np.array, pressure: np.array, rho: np.array, hij: np.array, cij: np.array, wij: np.array, dwij: np.array) -> float:
         """
             Monaghan Momentum equation
 
@@ -40,48 +39,59 @@ class Momentum():
 
             dwij: Kernel gradient
 
-            xsph: XSPH correction velocity.
-
-
 
             Returns:
             ------
 
-            If xsph is true a list is returned with [acceleration, xsph]
-
-            If xsph is false a list is returned with [acceleration]
+            acceleration
         """
 
         # Average density.
         rhoij: np.array = 0.5 * (p.rho + rho)
 
         # Compute first (easy) part.
-        tmp = p.p / (p.rho * p.rho) + pressure / (rho * rho)
-        _au = np.sum(- mass * tmp * dwij[:, 0], axis=0)
-        _av = np.sum(- mass * tmp * dwij[:, 1], axis=0)
+        _a = _acc_press(m, p.p, p.rho, pressure, rho, dwij)
 
-        # Diffusion
-        dot = np.sum(vij * xij, axis=1) # Row by row dot product
-        piij = np.zeros(len(pressure))
+        # Change in density due to diffusion.
+        _ad = _sum(_diff_vec(vij, xij, hij, rij, self.beta, self.alpha, cij, rhoij, m, dwij))
 
-        # Perform diffusion for masked entities
-        mask       = dot < 0
-        if any(mask):
-            muij       = hij[mask] * dot[mask] / (rij[mask] * rij[mask] + 0.01 * hij[mask] * hij[mask])
-            muij       = muij
-            piij[mask] = muij * (self.beta * muij - self.alpha * cij[mask])
-            piij[mask] = piij[mask] / rhoij[mask]
+        return _a + _ad
 
-        # Calculate change in density.
-        _au_d = np.sum(- mass * piij * dwij[:, 0])
-        _av_d = np.sum(- mass * piij * dwij[:, 1])
+@njit(fastmath=True)
+def _xsph(m: np.array, rho_s: float, rho_o: np.array, vij: np.array, wij: np.array) -> float:
+    _xsp = 0.0
+    I = len(vij)
+    for i in prange(I):
+        rhoij = 0.5 * (rho_s + rho_o)
+        _xsphtmp = m[i] / rhoij * wij[i]
+        _xsp += _xsphtmp * -vij[i]
+    return _xsp
 
-        # XSPH
-        if xsph == True:
-            _xsphtmp = mass / rhoij * wij
-            _xsphx = np.sum(_xsphtmp * -vij[:, 0], axis=0) # -vij = vji
-            _xsphy = np.sum(_xsphtmp * -vij[:, 1], axis=0)
+@njit(fastmath=True)
+def _acc_press(m: np.array, p_s: float, rho_s: float, p_o: np.array, rho_o: np.array, dwij: np.array) -> float:
+    I = len(p_o)
+    _a = 0.0
+    slf = p_s / (rho_s * rho_s)
+    for i in prange(I):
+        tmp = slf + p_o[i] / (rho_o[i] * rho_o[i])
+        _a += - m[i] * tmp * dwij[i]
+    return _a
 
-            return [np.array([_au + _au_d, _au + _av_d]), np.array([self.eta * _xsphx, self.eta * _xsphy])]
-        else:
-            return [np.array([_au + _au_d, _au + _av_d])]
+@vectorize(['float64(float64, float64, float64, float64, float64, float64, float64, float64, float64, float64)'], target='parallel')
+def _diff_vec(vij, xij, hij, rij, beta, alpha, cij, rhoij, m, dwij):
+    dot = vij * xij
+    if dot < 0.:
+        muij = hij * dot / (rij * rij + 0.01 * hij * hij)
+        ppij = muij * (beta * muij - alpha * cij)
+        piij = ppij / rhoij
+        return - m * piij * dwij
+    return 0.
+
+@njit(fastmath=True)
+def _sum(matrix: np.array):
+    _s = 0
+    I, J = matrix.shape
+    for i in prange(I):
+        for j in prange(J):
+            _s += matrix[i, j]
+    return _s
