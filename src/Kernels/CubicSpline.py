db60a36cf113886f3fa07baf9d117b874aca5229
Marielle Mandj Jasper <mandj@Marielles-iMac.local>
2019-02-07 17:48:56 +0100

    Cubic spline with spline tests.

--- src/Kernels/CubicSpline.py
+++ src/Kernels/CubicSpline.py
@@ -1,47 +1,69 @@
 import numpy as np
+from math import pow, pi
+from numba import vectorize
+from src.Kernels.Kernel import Kernel
 
+class CubicSpline(Kernel):
+    alpha: float
 
-class CubicSpline:
     def __init__(self):
-        M_1_PI = 1.0 / np.pi
+        self.alpha = 10 / (7 * pi)
 
-        self.dim = 2
-        self.fac = 10 * M_1_PI / 7.0
-
-    def evaluate(self, x: np.array, r: float, h: float):
+    def evaluate(self, r: np.array, h: np.array):
         """
         Evaluates the kernel function for the given points.
 
         Parameters
         ------
-        x: Distance vector
 
         r: Euclidian distance (scalar)
 
         h: Smoothing length
         """
-        h1 = 1. / h
-        q = r * h1
+        return _kernel(self.alpha, r, h)
+    
+    def gradient(self, x: np.array, r: np.array, h: np.array):
+        """
+        Calculates the gradient of the cubic spline at the given points.
 
-        # get the kernel normalizing factor
-        if self.dim == 1:
-            fac = self.fac * h1
-        elif self.dim == 2:
-            fac = self.fac * h1 * h1
-        elif self.dim == 3:
-            fac = self.fac * h1 * h1 * h1
+        Parameters
+        ------
 
-        tmp2 = 2. - q
+        x: Coordinate of the point (1D).
 
-        # q > 2.0
-        val = np.zeros(len(q))
+        r: Euclidian distance (scalar)
 
-        # q > 1.0
-        mask = [(q > 1.0) & (q < 2.0)]
-        val[mask] = 0.25 * tmp2[mask] * tmp2[mask] * tmp2[mask]
+        h: Smoothing length
+        """
+        return _gradient(self.alpha, x, r, h)
 
-        # q < 1.0
-        mask = [q < 1.0]
-        val[mask] = 1 - 1.5 * q[mask] * q[mask] * (1 - 0.5 * q[mask])
+@vectorize(['float64(float64, float64, float64)'], target='parallel')
+def _kernel(fac, r, h):
+    alpha = fac / (h * h)
+    q = r / h
 
-        return val * fac
+    # Three cases
+    if q > 2.0:
+        return 0.0
+    elif q > 1.0:
+        return alpha * 0.25 * pow(2 - q, 3)
+    else:
+        return alpha * (1 - 1.5 * pow(q, 2) * (1 - 0.5 * q))
+
+@vectorize(['float64(float64, float64, float64, float64)'], target='parallel')
+def _gradient(fac, x, r, h):
+    alpha = fac / (h * h)
+    q = r / h
+
+    # Calc derivative.
+    grad = 0.0
+    if (q > 2.0) or (r < 1e-10):
+        # Early exit, second condition for divide by zero.
+        return 0.0
+    elif q > 1.0:
+        grad = -0.75 * pow(2 - q, 2)
+    else:
+        grad = -3 * q * (1 - 0.75 * q)
+    
+    norm = alpha * grad / (h * r) # Normalize
+    return norm * x
