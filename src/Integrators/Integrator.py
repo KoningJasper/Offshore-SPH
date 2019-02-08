24d9d0eef871d2f4824f492a4fbf3bc1ca4c34b2
KoningJasper <mandj@Marielles-iMac.local>
2019-02-08 11:11:41 +0100

    PEC

--- src/Integrators/Integrator.py
+++ src/Integrators/Integrator.py
@@ -1,7 +1,38 @@
 import abc
+import numpy as np
 from src.Particle import Particle
 
 class Integrator():
+    __metaclass__ = abc.ABCMeta
+
+    """
+    
+    Integrator can have multiple passes.
+    
+    """
+
     @abc.abstractmethod
-    def integrate(self, dt: float, p: Particle) -> Particle:
+    def isMultiStage(self) -> bool:
+        """
+        Designates if the integrator is a multi-stage or single (evaluation) stage integrator. 
+        
+        A multi-stage integrator operates in the following cycle:
+        1. Evaluation
+        2. Predict
+        3. Evaluation
+        4. Correct
+
+        A single-stage integrator operates in the following cycle:
+        1. Predict
+        2. Evaluation
+        3. Correct
+        """
+        pass
+
+    @abc.abstractmethod
+    def predict(self, dt: float, p: np.array):
+        pass
+
+    @abc.abstractmethod
+    def correct(self, dt: float, p: np.array):
         pass
